dbb194dc81f1e0a83e507b12ad83228570baffe1
Milan Munko <milan.munko@ai-maps.com>
2016-08-30 18:21:14 +0200

    Added modification notices.

--- util/submit/stream.py
+++ util/submit/stream.py
@@ -13,6 +13,21 @@
 # language governing permissions and limitations under the License.
 #
 
+# NOTICE this file was modified 
+#
+# Copyright (C) 2016, AI-MAPS s. r. o.
+#
+# Author: Milan Muňko <milan.munko@ai-maps.com>
+#
+# All modifications, contributions and changes are:
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
+# writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
 __author__ = "sebastian.mattheis@bmw-carit.de"
 __copyright__ = "Copyright 2016 BMW Car IT GmbH"
 __license__ = "Apache-2.0"
